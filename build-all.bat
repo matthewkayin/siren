04551abf0cd48a5a3351160a611f5f41ab48e105
Matthew Madden <matthewkayin@gmail.com>
2024-02-18 10:40:34 -0800

    switch to using make for build system
    Signed-off-by: Matthew Madden <matthewkayin@gmail.com>

--- build-all.bat
+++ build-all.bat
@@ -1,16 +1,26 @@
-REM Build everything
 @ECHO OFF
+REM Build Everything
 
 ECHO "Building everything..."
 
-PUSHD engine
-CALL build.bat
-POPD
+
+@REM PUSHD engine
+@REM CALL build.bat
+@REM POPD
+@REM IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+@REM PUSHD testbed
+@REM CALL build.bat
+@REM POPD
+@REM IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+REM Engine
+make -f "makefile.engine.windows.mak" all
 IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
-PUSHD sandbox
-CALL build.bat
-POPD
+REM Sandbox
+make -f "makefile.sandbox.windows.mak" all
 IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
 
 ECHO "All assemblies built successfully."
